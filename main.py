4462324648b4c07734b8f38617ebddef18695726
Ian Moon <ianmoon20@gmail.com>
2024-10-29 02:50:11 -0400

    frame limiting implemented

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
         pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while(True):
         screen.fill((0,0,0))
@@ -16,6 +18,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        '''
+        Pause the game until 1/60th of a second has passed, then store the amount of time that's passed since the last time tick was called 
+        converted from ms to seconds.
+        '''
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
