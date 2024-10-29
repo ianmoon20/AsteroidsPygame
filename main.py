2e64552b9cf6cafe07034201c9d3e1ddd9645c5f
Ian Moon <ianmoon20@gmail.com>
2024-10-29 02:42:12 -0400

    Working window with close button. Also, added a .gitignore filter

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import pygame
+from constants import *
+
+def main():
+    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
+    if(pygame.get_init() == False):
+        pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while(True):
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
